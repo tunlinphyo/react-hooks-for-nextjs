ebd5257215a5ce3d2ede0a9c6defc07278b817a9
tunlinphyo <uniqueof1990@gmail.com>
2021-11-06 17:19:13 +0630

    useMedia

---
+++ media.js
@@ -0,0 +1,20 @@
+const { useState, useEffect } = require("react")
+
+export default function useMedia(queries, values, defaultValue) {
+  const [value, setValue] = useState(defaultValue)
+
+  useEffect(() => {
+    const mediaQueryLists = queries.map((q) => window.matchMedia(q))
+    const getValue = () => {
+      const index = mediaQueryLists.findIndex((mql) => mql.matches)
+      return typeof values[index] !== "undefined" ? values[index] : defaultValue
+    }
+    setValue(getValue)
+
+    const handler = () => setValue(getValue)
+    mediaQueryLists.forEach((mql) => mql.addListener(handler))
+    return () =>
+      mediaQueryLists.forEach((mql) => mql.removeListener(handler))
+  },[])
+  return value
+}
