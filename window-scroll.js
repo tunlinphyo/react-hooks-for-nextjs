84d07afb777e08fa72e03aec5b6cb07e66d4f306
tunlinphyo <uniqueof1990@gmail.com>
2021-11-06 17:51:13 +0630

    useWindowScroll

---
+++ window-scroll.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react"
+
+export default function useWindowScroll() {
+  const [ scroll, setScroll ] = useState(0)
+
+  useEffect(() => {
+    window.addEventListener('scroll', onScroll)
+    // window.addEventListener('wheel', onScroll)
+
+    return () => {
+      window.removeEventListener('scroll', onScroll)
+      // window.removeEventListener('wheel', onScroll)
+    }
+  }, [])
+
+  function onScroll() {
+    requestAnimationFrame(handleScroll)
+  }
+  function handleScroll() {
+    setScroll(window.scrollY)
+  }
+
+  return { scroll }
+}
