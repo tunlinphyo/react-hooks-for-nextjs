46be45d746bf770d17168eb739dfa4050c3cb2f8
tunlinphyo <uniqueof1990@gmail.com>
2021-11-08 02:22:59 +0630

    useModel

--- model.js
+++ model.js
@@ -6,8 +6,8 @@ export default function useModel(onClose, disableWheel=false) {
   useEffect(() => {
     const handkeKeydown = ({keyCode, metaKey, altKey}) => {
       if (
-        keyCode == 27 ||
-        ((metaKey || altKey) && keyCode == 37)
+        keyCode == 27 || // Escape Key
+        ((metaKey || altKey) && keyCode == 37) // Command or Alt + Back Arrow
       ) onClose()
     }
     disableScroll.on(null, { disableWheel })
