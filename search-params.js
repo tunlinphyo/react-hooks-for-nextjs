91a4e8a3c7c0823544a69d92869904735a658feb
tunlinphyo <uniqueof1990@gmail.com>
2021-11-09 06:42:01 +0630

    useSearchParams

---
+++ search-params.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react"
+
+export default function useSearchParams(query, callback) {
+  const [searchParams, setSearchParams] = useState(query)
+
+  useEffect(() => {
+    setSearchParams(query)
+  }, [query])
+ 
+  function getSearchParams(query) {
+    const newQuery = {}
+    Object.entries(query).forEach(([key, value]) => {
+      if (Array.isArray(value) ? value.length : value) newQuery[key] = value
+    })
+    const searchParams = new URLSearchParams(newQuery)
+    return searchParams.toString()
+  }
+
+  return {
+    searchParams,
+    getSearchParams
+  }
+}
